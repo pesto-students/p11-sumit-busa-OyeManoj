6d481ae0d58472ead4adb8c4e36d8d24e23a7a8e
Manoj Kumar Bhosale <82233618+OyeManoj@users.noreply.github.com>
2023-05-05 16:29:24 +0530

    update Session 9

---
+++ Week-3/Session 9/JavaScript arrays/script.js
@@ -0,0 +1,41 @@
+function divideArray(numbers) {
+    let evenNums = [];
+    let oddNums = [];
+  
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        evenNums.push(numbers[i]);
+      } else {
+        oddNums.push(numbers[i]);
+      }
+    }
+  
+    evenNums.sort(function(a, b) {
+      return a - b;
+    });
+  
+    oddNums.sort(function(a, b) {
+      return a - b;
+    });
+  
+    console.log("Even numbers:");
+    if (evenNums.length === 0) {
+      console.log("None");
+    } else {
+      for (let i = 0; i < evenNums.length; i++) {
+        console.log(evenNums[i]);
+      }
+    }
+  
+    console.log("Odd numbers:");
+    if (oddNums.length === 0) {
+      console.log("None");
+    } else {
+      for (let i = 0; i < oddNums.length; i++) {
+        console.log(oddNums[i]);
+      }
+    }
+  }
+  let nums = [4, 3, 9, 1, 24, 47, 73, 8];
+  divideArray(nums);
+    
